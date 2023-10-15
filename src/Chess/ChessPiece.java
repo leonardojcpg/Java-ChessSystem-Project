827cd369079db976972de190c0759aeea66bfa6f
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-10-14 23:52:03 -0300

    feat: Creating ChessMatch class

--- src/Chess/ChessPiece.java
+++ src/Chess/ChessPiece.java
@@ -1,5 +1,19 @@
 package Chess;
 
-public class ChessPiece {
+import BoardGame.Board;
+import BoardGame.Piece;
 
+public class ChessPiece extends Piece{
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	
 }
